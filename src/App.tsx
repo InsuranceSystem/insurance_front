1de7b5bf6a3619fd46bcf773c8eb43d33234bca6
lya0210 <lya8690@naver.com>
2023-11-05 00:03:44 +0900

    [feat] 초기 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './component/Header';
+import InsuranceList from './screen/Retreive/InsuranceList';
+import Application from './screen/Application';
+import MyContract from './screen/MyInsurance/MyContract';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<InsuranceList />} />
+          <Route path='/retrieve' element={<InsuranceList />} />
+          <Route path='/application' element={<Application />} />
+          <Route path='/management' element={<MyContract />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
